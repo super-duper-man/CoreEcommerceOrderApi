a738be89ceea5839274587ae31f9773f8aedcde3
Danny <m.r.fouladi7@gmail.com>
2025-11-08 13:02:27 +0330

    order entity/dto and product dto

---
+++ OrderApi.Application/Dtos/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record ProductDto(
+            int Id,
+            [Required] string Name,
+            [Required, Range(1, int.MaxValue)] string ProductQuantity,
+            [Required, DataType(DataType.Currency)] decimal Price
+        );
+}
---
+++ OrderApi.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderApi.Domain.Entities
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+        public int PurchaseQuantity { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+    }
+}
