756fdfe309734800fd0fec588e8da3161f3d89e2
Danny <m.r.fouladi7@gmail.com>
2025-11-08 13:59:09 +0330

    AppUserDto and OrderDetailsDto

---
+++ OrderApi.Application/Dtos/AppUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record AppUserDto(
+            int Id,
+            [Required] string Name,
+            [Required] string Phone,
+            [Required] string Address,
+            [Required, EmailAddress] string Email,
+            [Required] string password,
+            [Required] string role
+        );
+}
---
+++ OrderApi.Application/Dtos/OrderDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderApi.Application.Dtos
+{
+    public record OrderDetailsDto(
+            [Required] int OrderId,
+            [Required] int ProductId,
+            [Required] int ClientId,
+            [Required, EmailAddress] string Email,
+            [Required] string Phone,
+            [Required] string ProductName,
+            [Required] int PurchaseQuantity,
+            [Required, DataType(DataType.Currency)] decimal UnitPrice,
+            [Required, DataType(DataType.Currency)] decimal TotalPrice,
+            [Required] DateTime OrderDate
+        );
+}
