0c771562f7cb4ccd679d066ee5d106736a2d196e
Danny <m.r.fouladi7@gmail.com>
2025-11-09 15:23:58 +0330

    Order Controller initialize

---
+++ OrderApi.Presentation/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderApi.Application.Dtos;
+using OrderApi.Application.Dtos.Conversions;
+using OrderApi.Application.Interfaces;
+using OrderApi.Application.Services;
+using Resource.Share.Lib.Responses;
+
+namespace OrderApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController(IOrder orderInterface, IOrderService orderService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderDto>>> GetOrders()
+        {
+            var orders = await orderInterface.GetAllAsync();
+
+            if (!orders.Any())
+                return NotFound("No rder is in saerver");
+
+            var (_, _orders) = OrderConversion.FromEntity(null, orders);
+
+            return _orders!.Any() ? Ok(_orders) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response>> CreateOrder(OrderDto orderDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid Submitted Data!");
+
+            var order = OrderConversion.ToEntity(orderDto);
+
+            var response = await orderInterface.CreateAsync(order);
+
+            return response.Flag ? Ok(response) : BadRequest(response);
+        }
+    }
+}
--- OrderApi.Presentation/Program.cs
+++ OrderApi.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using OrderApi.Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddInfrastrctureService(builder.Configuration);
 var app = builder.Build();
+
+app.UseInfrastructurePolicty();
 
 app.UseSwagger();
 app.UseSwaggerUI();
