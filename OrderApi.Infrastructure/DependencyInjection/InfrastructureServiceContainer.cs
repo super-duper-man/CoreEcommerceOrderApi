fabad0af9b09dc987d13c5937412c909cac34ed8
Danny <m.r.fouladi7@gmail.com>
2025-11-09 09:33:27 +0330

    Infrasctructure ServiceContainer

---
+++ OrderApi.Infrastructure/DependencyInjection/InfrastructureServiceContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+using Resource.Share.Lib.DependencyInjection;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class InfrastructureServiceContainer
+    {
+        public static IServiceCollection AddInfrastrctureService(this IServiceCollection services, IConfiguration config)
+        {
+            ShareServiceContainer.AddShareService<OrderDbContext>(services, config, config["AppSerilog:FileName"]!);
+
+            services.AddScoped<IOrder, OrderRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicty(this IApplicationBuilder app)
+        {
+            ShareServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
