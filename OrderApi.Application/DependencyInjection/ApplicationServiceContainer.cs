ac4cd3f7e1d3b91893956e57bc6d70bd3ca2379d
Danny <m.r.fouladi7@gmail.com>
2025-11-09 08:56:18 +0330

    Application ServiceContainer class

---
+++ OrderApi.Application/DependencyInjection/ApplicationServiceContainer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Services;
+using Polly;
+using Polly.Retry;
+using Resource.Share.Lib.Logs;
+
+namespace OrderApi.Application.DependencyInjection
+{
+    public static class ApplicationServiceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection service, IConfiguration config)
+        {
+            service.AddHttpClient<IOrderService, OrderService>(options =>
+            {
+                options.BaseAddress = new Uri(config["ApiGateway:BaseAddress"]!);
+                options.Timeout = TimeSpan.FromSeconds(2);
+            });
+
+            //Create retry strategy
+            var retryStrategy = new RetryStrategyOptions()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<TaskCanceledException>(),
+                BackoffType = DelayBackoffType.Constant,
+                UseJitter = true,
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromMilliseconds(500),
+                OnRetry = arg =>
+                {
+                    string message = $"OnRetry Attempt: {arg.AttemptNumber} Outcome {arg.Outcome}";
+
+                    LogException.LogToConsole(message);
+                    LogException.LogToDebugger(message);
+
+                    return ValueTask.CompletedTask;
+                }
+            };
+            service.AddResiliencePipeline("retry-pipline", builder =>
+            {
+                builder.AddRetry(retryStrategy);
+            });
+
+            return service;
+        }
+    }
+}
